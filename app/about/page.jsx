97484aa9e6b403e255ab01a8292060d200d140ad
Deep Aghera <deep.aghera@marutitech.com>
2023-06-02 18:31:31 +0530

    first commit

---
+++ app/about/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+        <h1>About Traversy Media</h1>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur 
+            adipisicing elit. Enim rem pariatur magnam o
+            dit sint autem cupiditate deserunt fuga cum 
+            voluptates debitis animi accusantium dolores 
+            nostrum ratione vel, nihil ducimus ullam.
+        </p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ app/about/team/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TeamPage = () => {
+  return (
+    <div>
+        <h1>Our Team</h1>
+        <p>Lorem ipsum dolor sit amet consectetur 
+          adipisicing elit. Possimus nemo exercitationem adipisci at velit. 
+          Asperiores nesciunt placeat perspiciatis, fuga optio velit repellendus, eligendi deserunt rem fugit quam doloremque illo aliquam.</p>
+    </div>
+  )
+}
+
+export default TeamPage
